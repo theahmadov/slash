d550eda06735278c22931f04550a8074c3c380c4
Thesaderror <88781785+thesaderror@users.noreply.github.com>
2022-12-02 09:41:18 +0400

    Face Scrape

---
+++ api/faces.py
@@ -0,0 +1,23 @@
+import cv2
+import sys
+
+imagePath = "./images/image.jpeg"
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "./.db/haarcascade_frontalface_default.xml")
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.3,
+    minNeighbors=3,
+    minSize=(30, 30)
+)
+
+print("[INFO] Found {0} Faces!".format(len(faces)))
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+status = cv2.imwrite('faces_detected.jpg', image)
+print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
